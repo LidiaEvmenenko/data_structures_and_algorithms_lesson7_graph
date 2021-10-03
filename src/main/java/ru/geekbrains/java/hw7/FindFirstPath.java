c109121d3164d472fdeade29e117876cf8cfad69
LidiaEvmenenko <inu-chan@mail.ru>
2021-10-03 16:38:50 +0500

    Седьмое домашнее задание

---
+++ src/main/java/ru/geekbrains/java/hw7/FindFirstPath.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.java.hw7;
+
+import java.util.LinkedList;
+
+public class FindFirstPath {
+
+    protected boolean[] marked;
+    protected int[] edgeTo;
+    protected int source;
+
+    public FindFirstPath(Graph g, int source) {
+        this.source = source;
+        edgeTo = new int[g.getVertexCount()];
+        marked = new boolean[g.getVertexCount()];
+
+    }
+
+    public boolean hasPathTo(int dist) {
+        return marked[dist];
+    }
+
+    public LinkedList<Integer> pathTo(int dist) {
+        if (!hasPathTo(dist)) {
+            return null;
+        }
+        LinkedList<Integer> stack = new LinkedList<>();
+        int vertex = dist;
+        while (vertex != source) {
+            stack.push(vertex);
+            vertex = edgeTo[vertex];
+        }
+        return stack;
+    }
+
+}
--- src/main/java/ru/geekbrains/java/hw7/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/java/hw7/HomeWorkApp.java
@@ -3,7 +3,23 @@ package ru.geekbrains.java.hw7;
 public class HomeWorkApp {
 
     public static void main(String[] args) {
-       
+        Graph graph = new Graph(10);
+
+        graph.addEdge(0, 7);
+        graph.addEdge(0, 9);
+        graph.addEdge(1, 7);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(5, 7);
+        graph.addEdge(5, 9);
+        graph.addEdge(6, 8);
+        graph.addEdge(6, 9);
+        graph.addEdge(7, 9);
+
+        BreadthFirstPath dfp = new BreadthFirstPath(graph, 0);
+        System.out.println(dfp.pathTo(6));
 
     }
 
